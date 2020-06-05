7e0e29150968920d64127bef969751f0c64f25cd
Quentin Vanbutsele <vanbutsele@intechinfo.fr>
2020-06-05 16:47:39 +0200

    Create API points ; cleanup Backend

--- Server/api/models/Model.js
+++ Server/api/models/Model.js
@@ -1,12 +1,22 @@
 'use strict';
 
+//TODO : a game state with all the stuff !
+
 var Square = require('./Square'); //created model loading here
 
-
-class GameState {
+class Game {
     constructor(gridSize) {
       this.gridSize = gridSize;
     }
+    /*
+    player1 id (and score and turn ?)
+    player2 id
+    turn (who's playing ?)
+    grid
+    playTurn method to play a turn
+    
+    all the method to get the game working
+    */
   
     createNewGrid() {
       this.grid = Array(this.gridSize).fill(null).map(() => Array(this.gridSize).fill(Square.EmptySquare()));
@@ -24,4 +34,4 @@ class GameState {
     }
   }
   
-module.exports = GameState;
+module.exports = Game;
--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,7 @@ var express = require('express'),
   
   bodyParser = require('body-parser');
   
-
+//TODO : cleanup
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
